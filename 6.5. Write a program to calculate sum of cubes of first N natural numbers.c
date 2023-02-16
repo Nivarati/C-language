0e65abd030bd6327dfcecae74de67bfdf7f0fd63
Nivarati Chepile <118612416+Nivarati@users.noreply.github.com>
2023-02-16 20:27:20 +0530

    Add files via upload

---
+++ 6.5. Write a program to calculate sum of cubes of first N natural numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a=0,b=0,n;
+    printf("Enter n number to calculate sum of cubes of first N natural numbers\n");
+    scanf("%d",&n);
+    while(a<=n)
+
+    {
+        b=b+a*a*a;
+        a++;
+ 
+    }
+     printf("%d\n",b);
+    return 0;
+}
